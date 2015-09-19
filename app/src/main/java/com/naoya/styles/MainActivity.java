8477c2c70f619ac3bbaee6ed71467adffb810e32
Naoya Makino <naoyamakino@gmail.com>
2015-09-19 14:48:27 -0700

    first commit

---
+++ app/src/main/java/com/naoya/styles/MainActivity.java
@@ -0,0 +1,82 @@
+package com.naoya.styles;
+
+import android.animation.Animator;
+import android.annotation.TargetApi;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.ViewAnimationUtils;
+import android.widget.FrameLayout;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+    @Bind(R.id.frame1)
+    FrameLayout mFrameLayout1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    @OnClick(R.id.frame1) void onClickFrame() {
+        int radius = (int) Math.hypot(mFrameLayout1.getWidth()/2, mFrameLayout1.getHeight()/2);
+        Animator animator = ViewAnimationUtils.createCircularReveal(
+                mFrameLayout1,
+                (int) mFrameLayout1.getWidth()/2,
+                (int) mFrameLayout1.getHeight()/2,
+                0,
+                radius
+        );
+        mFrameLayout1.setBackground(
+                new ColorDrawable(
+                        getResources().getColor(R.color.green)));
+        animator.start();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
